e3ae717aa15e49e9a568e2e300d0545fe695b325
iWas-Coder <wasymatieh01@gmail.com>
2023-04-05 13:47:44 +0200

    Proxmox PRO Cluster provisioning

---
+++ src/cluster/production/proxmox/workers/worker-6.tf
@@ -0,0 +1,28 @@
+resource "proxmox_vm_qemu" "k8s-w6.pronghorn.org" {
+  name = "k8s-w6.pronghorn.org"
+  desc = "K8s Worker Node 6"
+  target_node = "proxmox"
+
+  clone = "debian-11"
+  agent = 1
+  cpu = "host"
+  sockets = 1
+  cores = 4
+  memory = 4096
+
+  network {
+    bridge = "vmbr0"
+    model = "virtio"
+  }
+
+  disk {
+    storage = "local-lvm"
+    type = "virtio"
+    size = "40G"
+  }
+
+  os_type = "cloud-init"
+  ipconfig0 = "ip=10.6.5.6/16,gw=10.6.1.1"
+  nameserver = "10.6.1.2"
+  sshkeys = pathexpand("key.pub")
+}
