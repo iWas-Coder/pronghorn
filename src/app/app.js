f4c1679484b3d52c5c5c7437dbccd5e3af249eee
iWas-Coder <wasymatieh01@gmail.com>
2023-07-01 20:10:23 +0200

    Demo app `pokedex` + updated gitignore

---
+++ src/app/app.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+const { version } = require('./package.json')
+
+const PORT = process.env.PORT || 5000
+
+app.get('/health', (req, res) => {
+  res.send('ok')
+})
+
+app.get('/version', (req, res) => {
+  res.send(version)
+})
+
+app.use(express.static('dist'))
+
+app.listen(PORT, () => {
+  console.log(`server started on port ${PORT}`)
+})
